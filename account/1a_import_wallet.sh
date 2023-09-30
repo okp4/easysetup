a46eda474b7e4dac5951f74f9f5c00e564af32a2
Anthony <10244943+antho31@users.noreply.github.com>
2023-10-01 00:33:26 +0200

    feat: add faucet comment on wallet creation

--- account/1a_import_wallet.sh
+++ account/1a_import_wallet.sh
@@ -1,2 +1,3 @@
 # Import from a mnemonic. You can replace "mywallet" with another wallet name
+# You must have $KNOW tokens to send a transaction - grab test tokens here: https://faucet.okp4.network/
 okp4d keys add --recover mywallet
--- account/1b_create_new_wallet.sh
+++ account/1b_create_new_wallet.sh
@@ -1,2 +1,3 @@
 # Create a new wallet. You can replace "mywallet" with another wallet name
+# You must have $KNOW tokens to send a transaction - grab test tokens here: https://faucet.okp4.network/
 okp4d keys add mywallet
