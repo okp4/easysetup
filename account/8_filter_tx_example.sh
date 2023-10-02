87ef6e629dbc31175ed086b05b97fe22fff517c1
Anthony Gourraud <10244943+antho31@users.noreply.github.com>
2023-10-02 11:48:34 +0300

    fix(scripts): reduce limit

--- account/8_filter_tx_example.sh
+++ account/8_filter_tx_example.sh
@@ -3,5 +3,5 @@
 okp4d query txs \
 --events 'transfer.recipient=okp41r0pf2d78w8w29sm9a6qm8x6yqshezm0k6vwcrg&transfer.amount=500000uknow' \
 --node https://api.testnet.okp4.network:443/rpc \
---page 1 --limit 1000 \
+--page 1 --limit 50 \
 --output json | jq '{total_count: .total_count, txs: [.txs[] | {txhash: .txhash, date: .timestamp, txdata: .logs[0].events[] | select(.type == "transfer").attributes}]}'
